5644b0f83111e44e02f2ad84ffc3d64bacd6bbb4
M1r0slav <72496884+M1r0slav@users.noreply.github.com>
2022-01-24 15:58:07 +0200

    implemented index page

--- MVC-App-BaseFrame-main/Application/Application/Controllers/ConcertsController.cs
+++ MVC-App-BaseFrame-main/Application/Application/Controllers/ConcertsController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Application.Controllers
 {
-    public class ProductsController : BaseController
+    public class ConcertsController : BaseController
     {
 
         public IActionResult Index()
